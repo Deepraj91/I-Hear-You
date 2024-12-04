19786d96f002b7913e53de009114eb313fb1051e
Deepraj91 <kushwahadeepraj91@gmail.com>
2024-12-04 19:45:18 +0530

    my 4th commit

--- app/src/main/java/com/example/ihearyou/LearningComponentsActivity.kt
+++ app/src/main/java/com/example/ihearyou/LearningComponentsActivity.kt
@@ -1,20 +1,58 @@
 package com.example.ihearyou
 
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.MediaController
+import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class LearningComponentsActivity : AppCompatActivity() {
+
+    // Declare VideoView variables
+    private lateinit var gujaratiVideo: VideoView
+    private lateinit var numbersVideo: VideoView
+    private lateinit var englishVideo: VideoView
+    private lateinit var moreVideo: VideoView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_learning_components)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Initialize video views
+        gujaratiVideo = findViewById(R.id.gujarati_video)
+        numbersVideo = findViewById(R.id.numbers_video)
+        englishVideo = findViewById(R.id.english_video)
+        moreVideo = findViewById(R.id.more_video)
+
+        // Setup video views with respective raw resources
+        setupVideoView(gujaratiVideo, R.raw.gujarati_alphabets)
+        setupVideoView(numbersVideo, R.raw.numbers)
+        setupVideoView(englishVideo, R.raw.alphabet)
+        setupVideoView(moreVideo, R.raw.math_signs)
+    }
+
+    /**
+     * Sets up a VideoView with a video file and a MediaController.
+     * @param videoView The VideoView to set up.
+     * @param rawResourceId The raw resource ID of the video file.
+     */
+    private fun setupVideoView(videoView: VideoView, rawResourceId: Int) {
+        val videoUri = Uri.parse("android.resource://${packageName}/$rawResourceId")
+        videoView.setVideoURI(videoUri)
+
+        // Add MediaController for playback controls
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        videoView.setMediaController(mediaController)
+
+        // Automatically start the video when it's clicked
+        videoView.setOnPreparedListener { mediaPlayer ->
+            mediaPlayer.setOnVideoSizeChangedListener { _, _, _ ->
+                videoView.start() // Start video playback
+            }
         }
+
+        // Focus the video view to ensure it displays properly
+        videoView.setZOrderOnTop(true)
     }
 }
